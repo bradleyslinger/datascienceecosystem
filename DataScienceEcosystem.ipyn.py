28e4f6153a93cfd9d9a2fe9341ac9a14caf53a61
bradleyslinger <52248122+bradleyslinger@users.noreply.github.com>
2023-06-11 23:02:31 +0100

    Add files via upload

---
+++ DataScienceEcosystem.ipyn.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# markdown_cell = """
+# # Data Science Tools and Ecosystem
+# """
+# 
+# print(markdown_cell)
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# markdown_cell = """
+# **Objectives:**
+# 
+# - List popular languages for Data Science
+# - List popular libraries for Data Science
+# - Introducing arithmetic expressions
+# - Multiplying and adding numbers
+# - Converting minutes to hours
+# """
+# 
+# print(markdown_cell)
+
+# markdown_cell = """
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R Language
+# 3. SQL
+# 4. Scala
+# 5. Java
+# 6. C++
+# """
+# 
+# print(markdown_cell)
+
+# markdown_cell = """
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Pandas
+# 2. NumPy
+# 3. Matplotlib
+# 4. Scikit-learn
+# 5. PyTorch
+# 6. TensorFlow
+# 7. Apache Spark
+# """
+# 
+# print(markdown_cell)
+
+# markdown_cell = """
+# | Data Science Tools |
+# | ----------------- |
+# | Jupyter Notebook  |
+# | RStudio           |
+# | PyCharm           |
+# """
+# 
+# print(markdown_cell)
+
+# markdown_cell = """
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+# """
+# 
+# print(markdown_cell)
+# 
+
+# In[1]:
+
+
+# This is a simple arithmetic expression to multiply then add integers.
+result = (3*4) + 5
+print(result)
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+hours = 200 / 60
+print(hours)
+
+
+# markdown_cell = """
+# ## Author
+# Bradley
+# """
+# 
+# print(markdown_cell)
